4904fc7f135e022d43e2e8ae91c32ff3ff75f51f
Giovanna <gio_ocz@hotmail.com>
2022-07-13 20:16:33 -0300

    menu

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,12 @@
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<template data-theme="mytheme">
+  <HelloWorld data-theme="mytheme" msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{vue,js,ts}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes:  [
+      {
+        mytheme: {
+          primary: "#283593",
+          secondary: "#3949ab",
+          accent: "#c1c8df",
+          neutral: "#1a237e",
+          "base-100": "#e8eaf6",
+          ghost: "#ffffff",
+          info: "#9DCEE2",
+          success: "#00e676",
+          warning: "#ffc107",
+          error: "#e53935",
+        },
+      },
+    ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('daisyui'),
+  ],
 }
