aa3fc62a2ef577401319308d61c9b963426e25b0
leticiabeluzi <leticia.beluzi@gmail.com>
2022-09-06 20:38:19 -0300

    add new client register view

---
+++ src/views/ClientRegister.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import Text from '../components/inputs/Text.vue'
+import Button from '../components/Button/Button.vue'
+import Container from '../components/Container/Container.vue'
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+export default {
+  name: 'ClientRegister',
+  components: {
+    Text,
+    Button,
+    Container
+  }
+}
+import vue from 'vue'
+</script>
+
+<template>
+  <Container>
+    <Text />
+    <Button></Button>
+  </Container>
+  <router-view />
+</template>
