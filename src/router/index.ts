71755b806679ac75888d515b3e533214973083e5
leticiabeluzi <leticia.beluzi@gmail.com>
2022-09-06 20:40:24 -0300

    client register router update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from "../views/Home.vue";
+import ClientRegister from "../views/ClientRegister.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/ClientRegister",
+    name: "ClientRegister",
+    component: ClientRegister,
   },
 ];
 
