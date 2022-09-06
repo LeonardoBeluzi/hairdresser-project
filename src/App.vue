dd49ebddce0f766ba7c576a4e1c457ac7e54153d
leticiabeluzi <leticia.beluzi@gmail.com>
2022-09-06 20:53:50 -0300

    add router-view to app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <script lang="ts">
-import './app.css'
-import TopNavigationBar from './components/navigation/TopNavigationBar.vue'
-import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router'
-export default {
-  name: 'App',
-  components: {
-    TopNavigationBar
+  import './app.css'
+  import TopNavigationBar from './components/navigation/TopNavigationBar.vue'
+  import { defineComponent } from 'vue'
+  import { useRouter } from 'vue-router'
+  export default {
+    name: 'App',
+    components: {
+      TopNavigationBar
+    }
   }
-}
-import vue from 'vue'
-</script>
-
-<template data-theme="main_theme">
-  <div id="app">
-    <TopNavigationBar></TopNavigationBar>
-  </div>
-  <router-view />
-</template>
-
-<style>
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-}
-</style>
+  import vue from 'vue'
+  </script>
+  
+  <template data-theme="main_theme">
+    <div id="app">
+      <TopNavigationBar></TopNavigationBar>
+    </div>
+    <router-view />
+  </template>
+  
+  <style>
+  #app {
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  </style>
+  
