d7f005d374f9a360bd77160a30b0bea55d813164
Giovanna <gio_ocz@hotmail.com>
2022-09-09 21:18:07 -0300

    add form control and full width

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { defineComponent } from 'vue'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { defineComponent } from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 </script>
 
 <template>
-  <button class="btn btn-primary w-28 my-8">Cadastrar</button>
+  <div class="form-control w-full max-w-xs mb-8">
+    <button class="btn btn-primary w-full my-6">Cadastrar</button>
+  </div>
 </template>
